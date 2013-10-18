021e6b32bfccb8c5faed50a3b0b986d87b5cbf6d
stupid3700 <smw@stumc.eu>
2013-10-18 09:29:08 +0100

    Fix up contributors page
    Correct links, add zacharycraft777 (@theboywithtrains)

--- contributers.php
+++ contributers.php
@@ -27,57 +27,63 @@
         <ul class="thumbnails">
           <li class="span2">
             <a href="http://oc.tc/MasterEjzz" class="thumbnail">
-            <img src="https://minotar.net/avatar/MasterEjzz/150" alt="MasterEjzz" width="150" height="150" /></a>
+            <img src="https://minotar.net/avatar/MasterEjzz/150" alt="MasterEjzz" width="150" height="150" />
             <p class="text-center">MasterEjzz</p>
             </a>  
           </li>
           <li class="span2">
             <a href="http://oc.tc/stupid3700" class="thumbnail">  
-            <img src="https://minotar.net/avatar/stupid3700/150" alt="stupid3700" width="150" height="150" /></a>
+            <img src="https://minotar.net/avatar/stupid3700/150" alt="stupid3700" width="150" height="150" />
             <p class="text-center">stupid3700</p>
             </a>  
           </li>
           <li class="span2">
             <a href="http://oc.tc/MathyBoy98" class="thumbnail">  
-            <img src="https://minotar.net/avatar/MathyBoy98/150" alt="MathyBoy98" width="150" height="150" /></a>
+            <img src="https://minotar.net/avatar/MathyBoy98/150" alt="MathyBoy98" width="150" height="150" />
             <p class="text-center">MathyBoy98</p>
             </a>  
           </li>
           <li class="span2">
             <a href="http://oc.tc/Troyter" class="thumbnail">  
-            <img src="https://minotar.net/avatar/Troyter/150" alt="Troyter" width="150" height="150" /></a>
+            <img src="https://minotar.net/avatar/Troyter/150" alt="Troyter" width="150" height="150" />
             <p class="text-center">Troyter</p>
             </a>  
           </li>
           <li class="span2">
             <a href="http://oc.tc/applenick3" class="thumbnail">  
-            <img src="https://minotar.net/avatar/applenick3/150" alt="applenick3" width="150" height="150" /></a>
+            <img src="https://minotar.net/avatar/applenick3/150" alt="applenick3" width="150" height="150" />
             <p class="text-center">applenick3</p>
             </a>  
           </li>
           <li class="span2">
             <a href="http://oc.tc/PhoenixMatt" class="thumbnail">  
-            <img src="https://minotar.net/avatar/PhoenixMatt/150" alt="PhoenixMatt" width="150" height="150" /></a>
+            <img src="https://minotar.net/avatar/PhoenixMatt/150" alt="PhoenixMatt" width="150" height="150" />
             <p class="text-center">PhoenixMatt</p>
-            </a>  
+            </a>
           </li>
           <li class="span2">
             <a href="http://oc.tc/Oversoul96" class="thumbnail">  
-            <img src="https://minotar.net/avatar/Oversoul96/150" alt="Oversoul96" width="150" height="150" /></a>
+            <img src="https://minotar.net/avatar/Oversoul96/150" alt="Oversoul96" width="150" height="150" />
             <p class="text-center">Oversoul96</p>
             </a>  
           </li>
           <li class="span2">
             <a href="http://oc.tc/mdahlgren" class="thumbnail">  
-            <img src="https://minotar.net/avatar/mdahlgren/150" alt="mdahlgren" width="150" height="150" /></a>
+            <img src="https://minotar.net/avatar/mdahlgren/150" alt="mdahlgren" width="150" height="150" />
             <p class="text-center">mdahlgren</p>
             </a>  
           </li>
           <li class="span2">
             <a href="http://oc.tc/goodbobul" class="thumbnail">  
-            <img src="https://minotar.net/avatar/GoodBobul/150" alt="GoodBobul" width="150" height="150" /></a>
+            <img src="https://minotar.net/avatar/GoodBobul/150" alt="GoodBobul" width="150" height="150" />
             <p class="text-center">GoodBobul</p>
             </a>  
+          </li>
+          <li class="span2">
+            <a href="https://oc.tc/zacharycraft777" class="thumbnail">
+            <img src="https://minotar.net/avatar/zacharycraft777/150" alt="zacharycraft777" width="150" height="150" />
+            <p class="text-center">zacharycraft777</p>
+            </a>
           </li>
         </ul>
         <hr>
