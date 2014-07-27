c8ed24f8b4b37861366f80aa07a27fe9289af53c
Ezekiel Elin <ezekielelin@me.com>
2014-07-27 07:56:23 -0400

    Updated Contributors page layout

--- contributors.php
+++ contributors.php
@@ -11,110 +11,170 @@ include("includes/functions.php");
   <link href="assets/css/style.css" rel="stylesheet" media="screen">
   <link href="assets/css/highlight.css" rel="stylesheet" media="screen">
   <link href="assets/ico/favicon.ico" rel="icon">
+  <style>
+    .row {
+      margin-top: 5px;
+      margin-bottom: 5px;
+    }
+  </style>
 </head>
 <body>
   <?php
   pageHeader("Home");
   ?>
-  <div class = "container">
-    <div class ="jumbotron">
+  <div class="container">
+    <div class="jumbotron">
       <h1>Contributors</h1>
       <p>The people who have helped make this wiki</p>
     </div>
-  </div>
-  <div class = "con">
-    <div class="container">
-      <ul class="thumbnails">
-        <li class="span2">
-          <a href="http://oc.tc/MasterEjzz" class="thumbnail">
-            <img src="https://minotar.net/avatar/MasterEjzz/150" alt="MasterEjzz" width="150" height="150" />
-            <p class="text-center">MasterEjzz</p>
-          </a>  
-        </li>
-        <li class="span2">
-          <a href="http://oc.tc/stupid3700" class="thumbnail">  
-            <img src="https://minotar.net/avatar/stupid3700/150" alt="stupid3700" width="150" height="150" />
-            <p class="text-center">stupid3700</p>
-          </a>  
-        </li>
-        <li class="span2">
-          <a href="http://oc.tc/MathyBoy98" class="thumbnail">  
-            <img src="https://minotar.net/avatar/MathyBoy98/150" alt="MathyBoy98" width="150" height="150" />
-            <p class="text-center">MathyBoy98</p>
-          </a>  
-        </li>
-        <li class="span2">
-          <a href="http://oc.tc/Troyter" class="thumbnail">  
-            <img src="https://minotar.net/avatar/Troyter/150" alt="Troyter" width="150" height="150" />
-            <p class="text-center">Troyter</p>
-          </a>  
-        </li>
-        <li class="span2">
-          <a href="http://oc.tc/applenick3" class="thumbnail">  
-            <img src="https://minotar.net/avatar/applenick3/150" alt="applenick3" width="150" height="150" />
-            <p class="text-center">applenick3</p>
-          </a>  
-        </li>
-        <li class="span2">
-          <a href="http://oc.tc/PhoenixMatt" class="thumbnail">  
-            <img src="https://minotar.net/avatar/PhoenixMatt/150" alt="PhoenixMatt" width="150" height="150" />
-            <p class="text-center">PhoenixMatt</p>
-          </a>
-          <li class="span2">
-            <a href="http://github.com/TheMasterCow" class="thumbnail">  
-              <img src="https://minotar.net/avatar/ParaSpider/150" alt="ParaSpider" width="150" height="150" />
-              <p class="text-center">ParaSpider</p>
-            </a>  
-          </li>
-          <li class="span2">
-            <a href="http://oc.tc/Oversoul96" class="thumbnail">  
-              <img src="https://minotar.net/avatar/Oversoul96/150" alt="Oversoul96" width="150" height="150" />
-              <p class="text-center">Oversoul96</p>
-            </a>  
-          </li>
-          <li class="span2">
-            <a href="http://oc.tc/mdahlgren" class="thumbnail">  
-              <img src="https://minotar.net/avatar/mdahlgren/150" alt="mdahlgren" width="150" height="150" />
-              <p class="text-center">mdahlgren</p>
-            </a>  
-          </li>
-          <li class="span2">
-            <a href="http://oc.tc/goodbobul" class="thumbnail">  
-              <img src="https://minotar.net/avatar/GoodBobul/150" alt="GoodBobul" width="150" height="150" />
-              <p class="text-center">GoodBobul</p>
-            </a>  
-          </li>
-          <li class="span2">
-            <a href="https://oc.tc/zacharycraft777" class="thumbnail">
-              <img src="https://minotar.net/avatar/zacharycraft777/150" alt="zacharycraft777" width="150" height="150" />
-              <p class="text-center">zacharycraft777</p>
-            </a>
-          </li>
-          <li class="span2">
-            <a href="http://github.com/BobRingard" class="thumbnail">  
-              <img src="https://minotar.net/avatar/BobRingard/150" alt="BobRingard" width="150" height="150" />
-              <p class="text-center">BobRingard</p>
-            </a>  
-          </li>
-          <li class="span2">
-            <a href="http://github.com/ezfe" class="thumbnail">  
-              <img src="https://minotar.net/avatar/ezekielelin/150" alt="ezfe" width="150" height="150" />
-              <p class="text-center">ezfe</p>
-            </a>  
-          </li>
-        </ul>
-        <hr>
+    <div class="row">
+      <div class="col-md-2 col-md-offset-4">
+        <a href="#">
+          <img src="https://avatar.oc.tc/MonsieurApple/128.png" alt="MonsieurApple">
+        </a>
+      </div>
+      <div class="col-md-2">
+        <h4>Coding and stuff</h4>
       </div>
     </div>
-    <div class="container">
-      <hr>
+    <div class="row">
+      <div class="col-md-2 col-md-offset-4">
+        <a href="http://oc.tc/MasterEjzz">
+          <img src="https://avatar.oc.tc/MasterEjzz/128.png" alt="MasterEjzz">
+        </a>
+      </div>
+      <div class="col-md-2">
+        <h4>MasterEjzz</h4>
+      </div>
     </div>
-    <script src="http://code.jquery.com/jquery.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script type="text/javascript"></script>
-    <?php
-    pageFooter();
-    ?>
+    <div class="row">
+      <div class="col-md-2 col-md-offset-4">
+        <a href="http://oc.tc/stupid3700">
+          <img src="https://avatar.oc.tc/stupid3700/128.png" alt="stupid3700">
+        </a>
+      </div>
+      <div class="col-md-2">
+        <h4>stupid3700</h4>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-2 col-md-offset-4">
+        <a href="http://oc.tc/MathyBoy98">
+          <img src="https://avatar.oc.tc/MathyBoy98/128.png" alt="MathyBoy98">
+        </a>
+      </div>
+      <div class="col-md-2">
+        <h4>MathyBoy98</h4>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-2 col-md-offset-4">
+        <a href="http://oc.tc/Troyter">
+          <img src="https://avatar.oc.tc/Troyter/128.png" alt="Troyter">
+        </a>
+      </div>
+      <div class="col-md-2">
+        <h4>Troyter</h4>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-2 col-md-offset-4">
+        <a href="http://oc.tc/applenick3">
+          <img src="https://avatar.oc.tc/applenick3/128.png" alt="applenick3">
+        </a>
+      </div>
+      <div class="col-md-2">
+        <h4>applenick3</h4>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-2 col-md-offset-4">
+        <a href="http://oc.tc/PhoenixMatt">
+          <img src="https://avatar.oc.tc/PhoenixMatt/128.png" alt="PhoenixMatt">
+        </a>
+      </div>
+      <div class="col-md-2">
+        <h4>PhoenixMatt</h4>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-2 col-md-offset-4">
+        <a href="http://github.com/TheMasterCow">
+          <img src="https://avatar.oc.tc/ParaSpider/128.png" alt="ParaSpider">
+        </a>
+      </div>
+      <div class="col-md-2">
+        <h4>ParaSpider</h4>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-2 col-md-offset-4">
+        <a href="http://oc.tc/Oversoul96">
+          <img src="https://avatar.oc.tc/Oversoul96/128.png" alt="Oversoul96">
+        </a>
+      </div>
+      <div class="col-md-2">
+        <h4>Oversoul96</h4>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-2 col-md-offset-4">
+        <a href="http://oc.tc/mdahlgren">
+          <img src="https://avatar.oc.tc/mdahlgren/128.png" alt="mdahlgren">
+        </a>
+      </div>
+      <div class="col-md-2">
+        <h4>mdahlgren</h4>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-2 col-md-offset-4">
+        <a href="http://oc.tc/goodbobul">
+          <img src="https://avatar.oc.tc/GoodBobul/128.png" alt="GoodBobul">
+        </a>
+      </div>
+      <div class="col-md-2">
+        <h4>GoodBobul</h4>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-2 col-md-offset-4">
+        <a href="http://oc.tc/zacharycraft777">
+          <img src="https://avatar.oc.tc/zacharycraft777/128.png" alt="zacharycraft777">
+        </a>
+      </div>
+      <div class="col-md-2">
+        <h4>zacharycraft777</h4>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-2 col-md-offset-4">
+        <a href="http://github.com/BobRingard">
+          <img src="https://avatar.oc.tc/BobRingard/128.png" alt="BobRingard">
+        </a>
+      </div>
+      <div class="col-md-2">
+        <h4>BobRingard</h4>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-2 col-md-offset-4">
+        <a href="http://github.com/ezfe">
+          <img src="https://avatar.oc.tc/ezekielelin/128.png" alt="ezekielelin">
+        </a>
+      </div>
+      <div class="col-md-2">
+        <h4>ezekielelin</h4>
+      </div>
+    </div>
+    <hr>
   </div>
+  <hr>
+  <script src="http://code.jquery.com/jquery.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script type="text/javascript"></script>
+  <?php
+  pageFooter();
+  ?>
 </body>
 </html>
