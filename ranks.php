10636ef658147d147aefae5dcc3b36322a49cbb1
Ezekiel Elin <ezekielelin@me.com>
2014-08-19 13:26:17 -0400

    Starting to convert ranks.php to a data driven format

--- ranks.php
+++ ranks.php
@@ -6,119 +6,37 @@
 	<?php assets(); ?>
 </head>
 <body>
-	<?php
-	pageHeader("ranks");
-	?>
+	<?php pageHeader("ranks"); ?>
 	<div class="container">
 		<div class ="jumbotron">
 			<h1>Ranks</h1>
-			<p>All the ranks on OCN. (Written by GoodBobul and ParaSpider)</p>
+			<p>All the ranks on OCN. (Written by GoodBobul and ParaSpider, Updated by Ezekiel)</p>
 		</div>
-		<hr>
-		<h2>Donation ranks</h2>
-		<hr>
-		<div class="media">
-			<a class="pull-left" href="#">
-				<span class="label" style="background-color: black; color: white">Decanus</span>
-			</a>
-			<div class="media-body">
-				<h4 class="media-heading">Decanus</h4>
-				<p>This used to be a black themed badge, but was recently deprecated as of December 23, 2013. It used to be found at <a href="http://oc.tc/shop">here</a>. It was $5 for 30 days of perks and it gave you a black badge on the website but no asterisk in game.</p>
-				<p>Example: <a href="http://i.imgur.com/1Bblvla.png">bleangamer</a></p>
+		<?php
+		$ranks = json_decode(file_get_contents('ranks.json'),true);
+		foreach ($ranks as $type => $type_ranks) {
+			?>
+			<div class="container">
+				<h2><?php echo $type; ?></h2>
 			</div>
-			<hr>
-		</div>
-		<div class="media">
-			<a class="pull-left" href="#">
-				<span class="label" style="background-color: green; color: white">Optio</span>
-			</a>
-			<div class="media-body">
-				<h4 class="media-heading">Optio</h4>
-				<p>You can buy this badge <a href="http://oc.tc/shop">here</a>. It is $10 for 60 days of perks. It gives you a green badge on the website and a green asterisk in-game.</p>
-				<p>Example: <a href="http://i.imgur.com/seNHdGA.png">Without_Regret</a></p>
-			</div>
-		</div>
-		<hr>
-		<div class="media">
-			<a class="pull-left" href="#">
-				<span class="label" style="background-color: #FDD017; color: black">Centurion</span>
-			</a>
-			<div class="media-body">
-				<h4 class="media-heading">Centurion</h4>
-				<p>You can buy this badge <a href="http://oc.tc/shop">here</a>. This gives you 180 days for $25. It also gives a yellow badge on the website and a yellow asterisk in game. The perks that donors got before the donor update are included in this package.</p>
-				<p>Example: <a href="http://i.imgur.com/kIaO0nJ.png">Magic_Mat</a></p>
-			</div>
-		</div>
-		<hr>
-		<div class="media">
-			<div class = "container">
-				<a class="pull-left" href="#">
-					<span class="label" style="background-color: purple; color: white">Dux</span>
-				</a>
-				<div class="media-body">
-					<h4 class="media-heading">Dux</h4>
-					<p>You can buy this badge <a href="http://oc.tc/shop">here</a>. It gives you unlimited donor perks for $50. You get a purple badge on the website and a purple asterisk in game. This gives you all of the perks that donors before the donation update got, including no expiry. However, it is five times the price.</p>
-					<p>Example: <a href="https://oc.tc/GoodBobul">GoodBobul</a></p>
+			<?php
+			foreach ($type_ranks as $index => $rank) {
+				?>
+				<div class="media">
+					<a class="pull-left" href="#">
+						<span class="label" style="background-color: <?php echo $rank['background-color']; ?>; color: <?php echo $rank['color']; ?>"><?php echo $rank['title']; ?></span>
+					</a>
+					<div class="media-body">
+						<h4 class="media-heading"><?php echo $rank['title']; ?></h4>
+						<p><?php echo $rank['body']; ?></p>
+						<p>Example: <a target="_" href="/assets/img/ranks/<?php echo $rank['example']; ?>.png"><?php echo $rank['example']; ?></a></p>
+					</div>
+					<hr>
 				</div>
-			</div>
-		</div>
-		<hr>
-		<div class="container">
-			<h2>Staff ranks</h2>
-		</div>
-		<hr>
-		<div class="media">
-			<div class = "container">
-				<a class="pull-left" href="#">
-					<span class="label" style="background-color: orange; color: white">Administrator</span>
-				</a>
-				<div class="media-body">
-					<h4 class="media-heading">Administrator</h4>
-					<p>An Admin has an orange/yellow badge on the forums and is represented by a yellow flair in-game. They can also moderate the forums and delete and lock topics. The admins are "in control" of the mods. We currently have six admins. Administrators have access to all operator commands and can moderate.</p>
-					<p>Example: <a href="https://oc.tc/MonsieurApple">MonsieurApple</a></p>
-				</div>
-			</div>
-		</div>
-		<hr>
-		<div class="media">
-			<div class = "container">
-				<a class="pull-left" href="#">
-					<span class="label" style="background-color: purple; color: white">Developer</span>
-				</a>
-				<div class="media-body">
-					<h4 class="media-heading">Developer</h4>
-					<p> This is someone who have helped with the coding of the plugin (PGM) or the website. They have a purple badge. They do not get an in-game flairs.</p>
-					<p>Example: <a href="https://oc.tc/Anxuiz">Anxuiz</a></p>
-				</div>
-			</div>
-		</div>
-		<hr>
-		<div class="media">
-			<div class = "container">
-				<a class="pull-left" href="#">
-					<span class="label" style="background-color: #F20; color: white">Moderator</span>
-				</a>
-				<div class="media-body">
-					<h4 class="media-heading">Moderator</h4>
-					<p>  A moderator has a bright red badge on the forums and is represented by a red star in game. They have the abilities of an admin in game except for promotion and other features of CommandBook. They cannot moderate the forums however take note that Overcast Network does not accept moderator requests and the admins choose who to mod next and invite them privately. Do not ask for mod as it greatly decreases your chances of getting it.</p>
-					<p>Example: <a href="https://oc.tc/Beebou">Beebou</a></p>
-				</div>
-			</div>
-		</div>
-		<hr>
-		<div class="media">
-			<div class = "container">
-				<a class="pull-left" href="#">
-					<span class="label" style="background-color: #F88; color: white">Junior Moderator</span>
-				</a>
-				<div class="media-body">
-					<h4 class="media-heading">Junior Moderator</h4>
-					<p>These are staff that are less experienced than normal moderators. It is basically a stepping stone for new moderators. The admins will see how mature each one is and decide whether or not to mod them completely. After a two week period, the final decision is made. They can do everything that moderators can do except end or setnext maps, and use WorldEdit commands (with the exception of few such as //drain).</p>
-					<p>Example: <a href="http://i.imgur.com/dNtMdYC.png">PlasmaCross</a></p>
-				</div>
-			</div>
-		</div>
-		<hr>
+				<?php
+			}
+		}
+		?>
 		<div class="media">
 			<div class = "container">
 				<a class="pull-left" href="#">
