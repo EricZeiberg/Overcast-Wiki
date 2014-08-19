6beae5ffc3c4d6dc1feb63a562c8c0339424820b
Ezekiel Elin <ezekielelin@me.com>
2014-08-19 13:42:54 -0400

    Did some changes to ranks layout

--- ranks.php
+++ ranks.php
@@ -8,25 +8,34 @@
 <body>
 	<?php pageHeader("ranks"); ?>
 	<div class="container">
-		<div class ="jumbotron">
-			<h1>Ranks</h1>
-			<p>All the ranks on OCN. (Written by GoodBobul and ParaSpider, Updated by Ezekiel)</p>
+		<div class="row">
+			<div class="col-md-12">
+				<div class ="jumbotron">
+					<h1>Ranks</h1>
+					<p>All the ranks on OCN. (Written by GoodBobul and ParaSpider, Updated by Ezekiel)</p>
+				</div>
+			</div>
 		</div>
 		<?php
+		###########################################
+		#TO EDIT THIS PAGE OPEN ranks.json INSTEAD#
+		###########################################
 		$ranks = json_decode(file_get_contents('ranks.json'),true);
 		foreach ($ranks as $type => $type_ranks) {
 			?>
-			<div class="container">
-				<h2><?php echo $type; ?></h2>
+			<div class="row">
+				<div class="col-md-10 col-md-offset-2">
+					<h1><?php echo $type; ?></h1>
+				</div>
 			</div>
 			<?php
 			foreach ($type_ranks as $index => $rank) {
 				?>
-				<div class="media">
-					<a class="pull-left" href="#">
+				<div class="row">
+					<div class="col-md-2" style="text-align:right;">
 						<span class="label" style="background-color: <?php echo $rank['background-color']; ?>; color: <?php echo $rank['color']; ?>"><?php echo $rank['title']; ?></span>
-					</a>
-					<div class="media-body">
+					</div>
+					<div class="col-md-10">
 						<h4 class="media-heading"><?php echo $rank['title']; ?></h4>
 						<p><?php echo $rank['body']; ?></p>
 						<?php
@@ -34,7 +43,7 @@
 							echo '<p>Examples: ';
 							foreach ($rank['example'] as $index => $user) {
 								?>
-									<a target="_" href="/assets/img/ranks/<?php echo $rank['example'][$index]; ?>.png"><?php echo $rank['example'][$index]; ?></a>
+								<a target="_" href="/assets/img/ranks/<?php echo $rank['example'][$index]; ?>.png"><?php echo $rank['example'][$index]; ?></a>
 								<?php
 							}
 							echo '</p>';
