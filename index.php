e466e4bcace6e86301b004c4db844359fd9504a7
Jake <jake0oo0andminecraft@gmail.com>
2014-11-17 21:24:42 -0600

    Add some reference links to the index page

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     </div>
     <hr>
     <div class="subtext">
-      <p>Overcast Network is a Minecraft PvP server started in December 2010 by MonsieurApple and Anxuiz. The server runs on the Bukkit API and is powered by many custom plugins, mainly PGM. Overcast also has a website where you can check your <a href="http://oc.tc/stats"> stats</a>, socialize with your friends on <a href="http://oc.tc/forums"> the forums</a> or just check <a href="http://oc.tc/revisions"> the revisions</a> for the latest changes to plugins, maps or the website.</p>
+      <p>Overcast Network is a Minecraft PvP server started in December 2010 by <a href="http://oc.tc/MonsieurApple">MonsieurApple</a> and <a href="http://oc.tc/Anxuiz">Anxuiz</a>. The server runs on the <a href="http://bukkit.org/">Bukkit API</a> and is powered by many custom plugins, mainly PGM. Overcast also has a website where you can check your <a href="http://oc.tc/stats"> stats</a>, socialize with your friends on <a href="http://oc.tc/forums"> the forums</a> or just check <a href="http://oc.tc/revisions"> the revisions</a> for the latest changes to plugins, maps or the website.</p>
     </div>
   </div>
   <?php pageFooter(); ?>
